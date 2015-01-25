2951bd2776f7bb084828afce1709d143dd29f51e
Anıl <anil.uz@outlook.com>
2015-01-25 04:41:07 +0200

    s4

--- text.js
+++ text.js
@@ -1,6 +1,6 @@
-    $(function(){
-        $(".innerdiv").typed({
+    $(function () {
+        $(".typing").typed({
             strings: [" hey passanger -^2000", " how ya doin -^2000", " i test on my code snippets here -^2000", " and so on -^2000", " have fun -^2000"],
-            typeSpeed: 100, 
+            typeSpeed: 100,
         });
     });
