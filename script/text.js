e621632cd24c4955263f5611ad76038cd327b40a
Anıl <anil.uz@outlook.com>
2015-01-27 23:00:22 +0200

    folderin

---
+++ script/text.js
@@ -0,0 +1,6 @@
+    $(function () {
+        $(".typing").typed({
+            strings: [" hey passanger -^2000", " how ya doin -^2000", " i test on my code snippets here -^2000", " and so on -^2000", " have fun -^2000"],
+            typeSpeed: 100,
+        });
+    });
